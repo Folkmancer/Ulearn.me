0570db6b5ac1e8db70def4992d9df0469795d254
Folkmancer <folkmancer@gmail.com>
2020-10-30 12:30:22 +0300

    Updating structure of solution

---
+++ BasicsOfProgrammingPartOne/BenfordStatistics/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BenfordStatistics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var res1 = GetBenfordStatistics("1");
+            var res2 = GetBenfordStatistics("abc");
+            var res3 = GetBenfordStatistics("123 456 789");
+            var res4 = GetBenfordStatistics("abc 123 def 456 gf 789 i");
+        }
+
+        //public static int[] GetBenfordStatistics(string text)
+        //{
+        //    var statistics = new int[10];
+
+        //    var words = text.Split(' ').Where(word => !string.IsNullOrEmpty(word) && char.IsDigit(word[0]));
+
+        //    foreach (var word in words)
+        //    {
+        //        var index = word[0] - '0';
+        //        statistics[index]++;
+        //    }
+
+        //    return statistics;
+        //}
+
+        public static int[] GetBenfordStatistics(string text)
+        {
+            var statistics = new int[10];
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (char.IsDigit(text[i]) && (i == 0 || !char.IsDigit(text[i - 1])))
+                {
+                    var index = text[i] - '0';
+                    statistics[index]++;
+                }
+            }
+
+            return statistics;
+        }
+
+        public static string ReplaceIncorrectSeparators(string text)
+        {
+            var separator = new char[] { ' ', ',', ';', '-' };
+            var splitedText = text.Split(separator, StringSplitOptions.RemoveEmptyEntries);
+
+            return string.Join("\t", splitedText);
+        }
+    }
+}
--- BasicsOfProgrammingPartOne/TextAnalysis/SentencesParserTask.cs
+++ BasicsOfProgrammingPartOne/TextAnalysis/SentencesParserTask.cs
@@ -1,6 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
+
 namespace TextAnalysis
 {
     static class SentencesParserTask
@@ -8,32 +8,42 @@ namespace TextAnalysis
         public static List<List<string>> ParseSentences(string text)
         {
             var sentencesList = new List<List<string>>();
-            foreach (var line in GetSentences(text))
+
+            foreach (var sentence in GetSentences(text))
             {
-                sentencesList.Add(GetWords(line));
+                var result = GetWords(sentence);
+                
+                if (result.Any())
+                    sentencesList.Add(result);
             }
+
             return sentencesList;
         }
 
         public static List<string> GetSentences(string text)
         {
             var sentences = new List<string>();
-            string output = new string(text.Where(c => !char.IsControl(c)).ToArray());
-            foreach (var line in output.Split('.', '!', '?', ';', ':', '(', ')'))
+
+            foreach (var sentence in text.Split('.', '!', '?', ';', ':', '(', ')'))
             {
-                if (line.Trim().Length != 0) sentences.Add(line);
+                if (!string.IsNullOrWhiteSpace(sentence)) 
+                    sentences.Add(sentence.Trim());
             }
+
             return sentences;
         }
 
         public static List<string> GetWords(string sentence)
         {
             var words = new List<string>();
-            string output = new string(sentence.Where(c => !char.IsControl(c)).Where(c => char.IsLetter(c)).ToArray());
-            foreach (var word in sentence.Split(' ').Where(c => !char.IsSeparator(c)))
+            var separators = sentence.Where(x => !char.IsLetter(x) && x != '\'').Distinct().ToArray();
+            
+            foreach (var word in sentence.Split(separators))
             {
-                words.Add(word);
+                if (!string.IsNullOrWhiteSpace(word))
+                    words.Add(word.Trim().ToLower());
             }
+
             return words;
         }
     }
